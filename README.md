46297e27956514202becdc5684b4cc415b70dbca
Michael DeSanty <michael.desanty@gmail.com>
2023-08-24 22:15:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advanced Web Dev React Tempate
+
+This is a template you can use for your React work in Advanced Web Development
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Replace me with your own stuff!</h1>
+    </>
+  );
+}
+
+export default App;
